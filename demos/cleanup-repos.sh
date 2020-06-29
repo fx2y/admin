ff03fe0b4d9097340d81a32f5401b7e3b644dc01
Andreas Evers <krosan@gmail.com>
2020-06-29 14:00:59 +0200

    New base sha

--- demos/cleanup-repos.sh
+++ demos/cleanup-repos.sh
@@ -28,6 +28,6 @@ cd ~/workspace-demo
 
 echo -e "${PURPLE}###### Resetting OPS repo${RESET_FONT}"
 git clone git@github.com:springone-tour-2020-cicd/go-demo-app-ops.git && cd go-demo-app-ops
-git reset --hard 6c9bb8d
+git reset --hard 152a91f
 git push -f
 cd ~/workspace-demo
